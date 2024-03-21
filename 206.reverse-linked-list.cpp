a6049cf6df7c8b35563ae3ed29652a953d1ba5b1
KiranDev12 <kirandev.edu@gmail.com>
2024-03-21 17:21:04 +0530

    GFG sorting start

---
+++ 206.reverse-linked-list.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *newHead = reverseLinkedList(head->next);
+        ListNode *front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
+// @lc code=end
